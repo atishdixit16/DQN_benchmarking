4e36c91000d5a80377ee9c6cc1e35eaa6ad327a9
Atish Narasinha Dixit <ad181@pet.hw.ac.uk>
2020-02-27 13:47:29 +0000

    check mountain car with target performance

--- data_viz.py
+++ data_viz.py
@@ -3,12 +3,17 @@ import matplotlib.pyplot as plt
 
 ENV = 'CartPole-v0'
 CASE_1_TITLE = 'with target network'
-CASE_2_TITLE = 'withour target network'
+CASE_2_TITLE = 'without target network'
 
-CASE_1_PATH = 'with_target_results/'
-CASE_2_PATH = 'without_target_results/'
+CASE_1_PATH = 'target_network_effect_study/with_target_results_20000/'
+CASE_2_PATH = 'target_network_effect_study/without_target_results_20000/'
 CASE_1_CSV_INITIALS = 'expt'
 CASE_2_CSV_INITIALS = 'expt'
+
+CASE_1_TIME = '15 mins'
+CASE_2_TIME = '14.5 mins'
+
+PLOT_SAVE_PATH = 'target_network_effect_study/plots_20000/'
 
 file_path_1 = CASE_1_PATH + CASE_1_CSV_INITIALS
 file_path_2 = CASE_2_PATH + CASE_2_CSV_INITIALS
@@ -60,11 +65,11 @@ plt.fill_between(x, np.nanpercentile(y_openAI, 25, axis=0), np.nanpercentile(y_o
 plt.plot(x, np.nanmedian(y_in_house, axis=0) )
 plt.fill_between(x, np.nanpercentile(y_in_house, 25, axis=0), np.nanpercentile(y_in_house, 75, axis=0), alpha=0.25)
 plt.grid('True')
-plt.legend(['openAI baseline code', 'DQN vanilla'])
+plt.legend([CASE_1_TITLE, CASE_2_TITLE])
 plt.xlabel('Episodes')
 plt.ylabel('Rewards')
-plt.title('MountainCar-v0 case comparison \n computation time per expmt \n openAI baseline (~1 h 12 mins); DQN vanilla (~10 mins)')
-plt.savefig('plots/'+str(version)+'/comparison.png')
+plt.title('Environment: '+ENV+'\n mean computation time per expmt: ' +CASE_1_TITLE+' (' +CASE_1_TIME +'); ' + CASE_2_TITLE + ' (' +CASE_2_TIME+') ')
+plt.savefig(PLOT_SAVE_PATH+'comparison.png')
 
 # data vizualization : exploration for episodes in OpenAI experiments
 plt.clf()
@@ -74,12 +79,12 @@ plt.fill_between(x, np.nanpercentile(y_openAI, 25, axis=0), np.nanpercentile(y_o
 plt.grid('True')
 plt.xlabel('Episodes')
 plt.ylabel('Rewards')
-plt.title('Exploration in openAI experiments')
+plt.title('Exploration in case: '+CASE_1_TITLE)
 ax2 = plt.twinx()
 plt.plot(x, np.nanmedian(eps_openAI, axis=0), color='r' )
 plt.fill_between(x, np.nanpercentile(eps_openAI, 25, axis=0), np.nanpercentile(eps_openAI, 75, axis=0), alpha=0.25, color='r')
 plt.ylabel('% time spent exploring')
-plt.savefig('plots/'+str(version)+'/open_AI_baseline.png')
+plt.savefig(PLOT_SAVE_PATH+'open_AI_baseline.png')
 
 
 # data vizualization : exploration for episodes in vanilla DQN experiments
@@ -90,10 +95,10 @@ plt.fill_between(x, np.nanpercentile(y_in_house, 25, axis=0), np.nanpercentile(y
 plt.grid('True')
 plt.xlabel('Episodes')
 plt.ylabel('Rewards')
-plt.title('Exploration in vanilla DQN experiments')
+plt.title('Exploration in case: '+CASE_2_TITLE)
 ax2 = plt.twinx()
 plt.plot(x, np.nanmedian(eps_in_house, axis=0), color='r' )
 plt.fill_between(x, np.nanpercentile(eps_in_house, 25, axis=0), np.nanpercentile(eps_in_house, 75, axis=0), alpha=0.25, color='r')
 plt.ylabel('% time spent exploring')
-plt.savefig('plots/'+str(version)+'/vanilla_dqn.png')
+plt.savefig(PLOT_SAVE_PATH+'vanilla_dqn.png')
 
